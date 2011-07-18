c8fc899520ae4375871fcec4ca3ffccc8b252174
brefsdal <brefsdal@1f3b12eb-0b44-1d3a-2961-b75907fe8195>
2011-07-18 17:53:58 +0000

    license stuff, bump version

--- sherpa-samp/setup.py
+++ sherpa-samp/setup.py
@@ -21,7 +21,7 @@
 from distutils.core import setup
 
 setup(name='sherpa_samp',
-      version='0.1',
+      version='0.2',
       author='Smithsonian Astrophysical Observatory / Chandra X-Ray Center',
       author_email='cxchelp@head.cfa.harvard.edu',
       url='http://cxc.harvard.edu/sherpa/',
